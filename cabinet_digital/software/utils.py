f3d4e3a850a8ea98ac23a0493a53d2165f8f34df
Arthur <arthurjoubin@gmail.com>
2024-12-07 16:50:00 +0100

    maj

--- cabinet_digital/software/utils.py
+++ cabinet_digital/software/utils.py
@@ -11,16 +11,21 @@ def capture_website_scroll(url, output_path):
     
     # Configuration de Chrome en mode headless
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--ignore-certificate-errors")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
     
-    driver = webdriver.Chrome(options=chrome_options)
+    service = webdriver.ChromeService()
+    driver = webdriver.Chrome(options=chrome_options, service=service)
     try:
         driver.get(url)
         time.sleep(3)  # Attendre le chargement
